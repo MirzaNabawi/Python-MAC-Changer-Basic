69919e9d85542ac2f99af5703e59d7dc57cdbb3e
KaaloChele <nabawizaman20@gmail.com>
2024-04-28 15:02:04 +0600

    MAC Changer Command option 2 (making it safer)

--- import subprocess.py
+++ import subprocess.py
@@ -8,9 +8,9 @@ new_mac= input("new_mac > ")
 print("[+]Changing MAC address " + interface + " to " + new_mac )
 
 
-subprocess.call(["ifconfig ", interface , " down "], shell=True)
-subprocess.call(["ifconfig ", interface , " hw "," ether "], shell=True)
-subprocess.call(["ifconfig ", interface , " up "], shell=True)
+subprocess.call(["ifconfig ", interface , " down "])
+subprocess.call(["ifconfig ", interface , " hw "," ether "])
+subprocess.call(["ifconfig ", interface , " up "])
 
 
 
