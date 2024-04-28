8f3d976cc84216128f5b20b6e8b65be307583caf
KaaloChele <nabawizaman20@gmail.com>
2024-04-28 16:57:54 +0600

    Using [optparse] module along with python function to Simplify MAC changer further

--- import subprocess.py
+++ import subprocess.py
@@ -1,23 +1,20 @@
 import subprocess
 import optparse
 
-subprocess.call("ifconfig")
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address of ", interface, " to ", new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-parser= optparse.OptionParser()
+parser = optparse.OptionParser()
 parser.add_option("-i","--interface", dest = "interface" , help = "Interface to change MAC Address")
 parser.add_option("-m","--mac_address",dest = "new_mac",help = "New MAC address")
 
 (options, arguments)=parser.parse_args()
 
-interface= options.interface
-new_mac= options.new_mac
+change_mac(options.interface, options.new_mac)
 
-print("[+]Changing MAC address " + interface + " to " + new_mac )
-
-
-subprocess.call(["ifconfig", interface , "down"])
-subprocess.call(["ifconfig", interface , "hw" , "ether" , new_mac])
-subprocess.call(["ifconfig", interface , "up"])
 
 
 
