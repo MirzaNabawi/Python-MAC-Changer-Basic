02d43a965e29489c072059d10cfb968e0e73c8d8
KaaloChele <nabawizaman20@gmail.com>
2024-04-28 17:51:43 +0600

    Implementing Python Functions for both Argument and Option in MAC changer

--- import subprocess.py
+++ import subprocess.py
@@ -1,18 +1,20 @@
 import subprocess
 import optparse
 
+def argument():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface", dest = "interface" , help = "Interface to change MAC Address")
+    parser.add_option("-m","--mac_address",dest = "new_mac",help = "New MAC address")
+
+    return parser.parse_args()
+
 def change_mac(interface, new_mac):
     print("[+] Changing MAC Address of ", interface, " to ", new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-parser = optparse.OptionParser()
-parser.add_option("-i","--interface", dest = "interface" , help = "Interface to change MAC Address")
-parser.add_option("-m","--mac_address",dest = "new_mac",help = "New MAC address")
-
-(options, arguments)=parser.parse_args()
-
+(options,arguments)=argument()
 change_mac(options.interface, options.new_mac)
 
 
