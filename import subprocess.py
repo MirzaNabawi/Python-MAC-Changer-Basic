8980701e1790e3af5271d12679f82b630c372b32
KaaloChele <nabawizaman20@gmail.com>
2024-05-01 04:25:41 +0600

    MAC Changer  (New Version)

--- import subprocess.py
+++ import subprocess.py
@@ -1,45 +1,43 @@
 import subprocess
 import optparse
 import re
-
-def argument():
-    parser = optparse.OptionParser()
-    parser.add_option("-i","--interface", dest = "interface" , help = "Interface to change MAC Address")
-    parser.add_option("-m","--mac_address",dest = "new_mac",help = "New MAC address")
-    (options, argument)= parser.parse_args()
+def user_interface():
+    parser= optparse.OptionParser()
+    parser.add_option( "-i" , dest= "interface" , help = "Give Interface")
+    parser.add_option( "-m" , dest= "new_mac" , help = "Give MAC Address")
+    (options,arguments)=parser.parse_args()
     if not options.interface:
-        parser.error("Please specify a proper Interface or use --help for more information")
+        parser.error("[+] Please Specify Interface> ")
     elif not options.new_mac:
-        parser.error("Please specify a proper MAC Address or use --help for more information")
-    
-    return options 
-
-
-def change_mac(interface, new_mac):
-    print("[+] Changing MAC Address of ", interface, " to ", new_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-
+        parser.error("[+] Please Specify MAC Address")
+    return options
+def change_mac(interface,new_mac):
+    subprocess.call(["ifconfig", interface , "down"])
+    subprocess.call(["ifconfig", interface , "hw","ether" , new_mac])
+    subprocess.call(["ifconfig", interface , "up"])
 def get_current_mac(interface):
-    ifconfig_result=str(subprocess.check_output(["ifconfig",interface]),"ascii")
-
-    mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig_result)
-    if mac_address_result:
-            return(mac_address_result.group(0))
+    ifconfig_result=str(subprocess.check_output(["ifconfig", interface]),"ascii")
+    mac_address=re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig_result)
+    if mac_address:
+        return (mac_address.group(0))
     else:
-            print("[-] Could not find MAC Address")
-
-options=argument()
-current_mac=get_current_mac(options.interface) 
-print("Current MAC Address is " + str(current_mac))
-
-change_mac(options.interface, options.new_mac)
-
+        print("[+] Could Not find MAC Address")
+options=user_interface()
+current_mac=get_current_mac(options.interface)
+print("User interface is " , options.interface)
+print("Current MAC is",current_mac)
+change_mac(options.interface,options.new_mac)
+current_mac=get_current_mac(options.interface)
 if current_mac == options.new_mac:
-     print("[+] MAC Address did not change ")
+    print("New MAC Address " , current_mac)
 else:
-     print("MAC Address Changed to " + options.new_mac)
+    print("[+] MAC Address did not change ")
+
+
+
+
+
+
 
 
 
